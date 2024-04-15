6ab7793ab3cfea290470cb546f5379b9c909e5e1
Samuel Lanzuela <amlanzuela@gmail.com>
2024-04-15 13:52:59 -0700

    w

--- GDBLanzuela/Assets/SpawnOverTime.cs
+++ GDBLanzuela/Assets/SpawnOverTime.cs
@@ -7,9 +7,14 @@ public class SpawnOverTime : MonoBehaviour
     public float timer;
     float timerInit;
     public GameObject objecty;
+    public int patientsInit;
     // Start is called before the first frame update
     void Start()
     {
+        for (int i  = 0; i < patientsInit; i++)
+        {
+            Instantiate(objecty, transform.position, Quaternion.identity);
+        }
         timerInit = timer;
     }
 
